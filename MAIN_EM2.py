fbca7b08b769424991ad48d09cc634bb7a2f6cc8
ArtBI11 <artyombirulya@gmail.com>
2026-08-26 15:25:30 +0300

    Добавил функции рассылки и остальные функции. Изменил им названия и у некоторый приписал users или meet

--- MAIN_EM2.py
+++ MAIN_EM2.py
@@ -8,7 +8,98 @@ from tkinter import messagebox
 from tkinter import ttk
 
 CSV_FILE_MEET = "Meetings.csv"
+CSV_FILE = "users.csv"
 
+def load_data_from_csv():
+    """Функция загрузки данных из CSV-файла."""
+    if not os.path.exists(CSV_FILE):
+        create_test_csv_users()
+
+    data = []
+    try:
+        with open(CSV_FILE, mode="r", encoding="utf-8-sig") as f:
+            reader = csv.reader(f, delimiter=";")
+            next(reader)  # Пропускаем заголовок
+
+            for row in reader:
+                if row:  # Проверка на пустые строки
+                    data.append(row)
+    except Exception as e:
+        messagebox.showerror(
+            "Ошибка", f"Не удалось прочитать файл {CSV_FILE}:\n{e}"
+        )
+    return data
+
+
+def create_test_csv_users():
+    """Создает тестовый файл, если его не существует."""
+    test_data = [
+        ["id", "name", "email", "role"],
+        ["1", "Алексей", "artyombirulya@gmail.com", "Админ"],  # Ваш email для теста
+        ["2", "Мария", "maria@example.com", "Пользователь"],
+        ["3", "Иван", "ivan@example.com", "Модератор"],
+    ]
+    with open(CSV_FILE, mode="w", encoding="utf-8-sig", newline="") as f:
+        writer = csv.writer(f, delimiter=";")
+        writer.writerows(test_data)
+
+
+
+def send_emails():
+    """Функция рассылки писем по всему списку из таблицы."""
+    all_items = tree.get_children()
+
+    if not all_items:
+        messagebox.showwarning("Внимание", "Список пользователей пуст!")
+        return
+
+    sent_count = 0
+
+    # Проходим циклом по каждой строке таблицы
+    for item in all_items:
+        values = tree.item(item, "values")
+        user_name = values[1]
+        user_email = values[2]
+
+        print(f"Отправка на {user_email}: Hello, {user_name}!")
+
+        # НАСТРОЙКИ ЯНДЕКСА
+        SENDER = "artmbirulya@yandex.com"  # Изменили .com на .ru
+        PASSWORD = "fiwvbwzisbfnincq"  # Ваш 16-значный пароль приложения
+        RECIPIENT = user_email
+
+        # Создание письма
+        msg = EmailMessage()
+        msg["Subject"] = "Тестовое письмо"
+        msg["From"] = SENDER
+        msg["To"] = RECIPIENT
+        msg.set_content(
+            f"Hello, {user_name}!\n\nУра! Код на Python работает, и письмо успешно отправлено!"
+        )
+
+        # Отправка через сервер Яндекса
+        try:
+            with smtplib.SMTP_SSL("smtp.yandex.ru", 465) as server:
+                server.set_debuglevel(1)  # Логи включаются ДО авторизации
+                server.login(SENDER, PASSWORD)
+                server.send_message(msg)
+
+            print(f"Письмо для {user_name} успешно отправлено!")
+            sent_count += 1  # Считаем только успешные
+
+        except Exception as e:
+            print(f"Произошла ошибка при отправке на {user_email}: {e}")
+            messagebox.showerror(
+                "Произошла ошибка при отправке",
+                f"Не удалось отправить письмо для {user_name} ({user_email}):\n{e}",
+            )
+
+    # ОКНО УВЕДОМЛЕНИЯ (Вынесено из цикла наружу)
+    if sent_count > 0:
+        messagebox.showinfo(
+            "Рассылка завершена",
+            f"Успешно отправлено писем: {sent_count}.",
+        )
 
 
 
@@ -20,48 +111,48 @@ CSV_FILE_MEET = "Meetings.csv"
 placeholder_map = {}
 
 
-# def load_data_from_csv():
-#     """Функция загрузки данных из CSV-файла."""
-#     # 1. Проверяем, существует ли файл
-#     if not os.path.exists(CSV_FILE_MEET):
-#         choice = messagebox.askyesno(
-#             "Не найден файл Meetings.csv", "Создать файл с шаблоном?"
-#         )
-#         if choice:
-#             create_test_csv()  # Создаем файл
-#         else:
-#             root.destroy()
-#             sys.exit()
+def load_data_from_csv_meet():
+    """Функция загрузки данных из CSV-файла."""
+    # 1. Проверяем, существует ли файл
+    if not os.path.exists(CSV_FILE_MEET):
+        choice = messagebox.askyesno(
+            "Не найден файл Meetings.csv", "Создать файл с шаблоном?"
+        )
+        if choice:
+            create_test_csv_meet()  # Создаем файл
+        else:
+            root.destroy()
+            sys.exit()
 
-#     data = []
-#     try:
-#         with open(CSV_FILE_MEET, mode="r", encoding="utf-8-sig") as f:
-#             reader = csv.reader(f, delimiter=";")
-#             next(reader)  # Пропускаем заголовок
+    data = []
+    try:
+        with open(CSV_FILE_MEET, mode="r", encoding="utf-8-sig") as f:
+            reader = csv.reader(f, delimiter=";")
+            next(reader)  # Пропускаем заголовок
 
-#             for row in reader:
-#                 if row:  # Проверка на пустые строки
-#                     data.append(row)
-#     except Exception as e:
-#         messagebox.showerror(
-#             "Ошибка", f"Не удалось прочитать файл {CSV_FILE_MEET}:\n{e}"
-#         )
+            for row in reader:
+                if row:  # Проверка на пустые строки
+                    data.append(row)
+    except Exception as e:
+        messagebox.showerror(
+            "Ошибка", f"Не удалось прочитать файл {CSV_FILE_MEET}:\n{e}"
+        )
 
-#     return data
+    return data
     
 
 
-# def create_test_csv():
-#     #Создает тестовый файл, если его не существует.
-#     test_data = [
-#         ["name_of_meet", "end_date", "start_date"],
-#         ["Алексей", "02.04.26", "02.04.26"],  # Ваш email для теста
-#         ["Мария", "02.04.26", "02.04.26"],
-#         ["Иван", "02.04.26", "02.04.26"],
-#     ]
-#     with open(CSV_FILE_MEET, mode="w", encoding="utf-8-sig", newline="") as f:
-#         writer = csv.writer(f, delimiter=";")
-#         writer.writerows(test_data)
+def create_test_csv_meet():
+    #Создает тестовый файл, если его не существует.
+    test_data = [
+        ["name_of_meet", "end_date", "start_date"],
+        ["Алексей", "02.04.26", "02.04.26"],  # Ваш email для теста
+        ["Мария", "02.04.26", "02.04.26"],
+        ["Иван", "02.04.26", "02.04.26"],
+    ]
+    with open(CSV_FILE_MEET, mode="w", encoding="utf-8-sig", newline="") as f:
+        writer = csv.writer(f, delimiter=";")
+        writer.writerows(test_data)
 
 def enter_function_input(event):
     text_placeholder = placeholder_map.get(event.widget)
@@ -118,6 +209,8 @@ placeholder_map[entry_data] = "Дата..."
 entry_data.bind("<FocusIn>", on_entry_click)
 entry_data.bind("<FocusOut>",off_entry_mouse)
 entry_name.bind("<Return>",enter_function_input)
+
+
 
 # label_title = tk.Label(root, text="Список запланированных мероприятий",font=("Arial",12,"bold"))
 # label_title.pack(pady=(20,0))
